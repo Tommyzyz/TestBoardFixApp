582ac7f7dd00739980346c853905e14922323a72
Tommyzyz <tommyzhao2017@outlook.com>
2023-12-05 22:53:55 +0800

    加入数据库,完成添加数据功能

---
+++ TestBoardFixApp/Model/EntityConfig.cs
@@ -0,0 +1,19 @@
+﻿
+
+
+
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace TestBoardFixApp.Model;
+
+public class EntityConfig : IEntityTypeConfiguration<FixFileData>
+{
+    public void Configure(EntityTypeBuilder<FixFileData> builder)
+    {
+        builder.ToTable("T_FixFileData");
+        builder.Property(e => e.TestMachingType).IsRequired();
+        builder.Property(e=>e.TestMachingNum).HasMaxLength(50).IsRequired();
+
+    }
+}
+
