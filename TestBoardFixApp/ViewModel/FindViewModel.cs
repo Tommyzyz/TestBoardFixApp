6138f3412f92a6ae334b0e4e2f3e06f3b4441efb
Tommyzyz <tommyzhao2017@outlook.com>
2023-12-17 21:05:43 +0800

    增加高级搜索

--- TestBoardFixApp/ViewModel/FindViewModel.cs
+++ TestBoardFixApp/ViewModel/FindViewModel.cs
@@ -9,6 +9,43 @@ public partial class FindViewModel:ObservableObject
 
     //[ObservableProperty]
     //private List<FixFileData> selectedFixFile = new();
+    [ObservableProperty]
+    private string? selectedTestMachingType;
+
+    [ObservableProperty]
+    private string? selectedBoardName;
+
+    [ObservableProperty]
+    private string? selectedFixWay;
+
+    [ObservableProperty]
+    private DateTime selectedStartDate;
+
+    [ObservableProperty]
+    private DateTime selectedEndDate;
+
+    [RelayCommand]
+    private void AdvancedSearch()
+    {
+        using TestDbContext db = new TestDbContext();
+        {
+            IQueryable<FixFileData> SelectedFile = db.FixFileData.Where(item => item.StartFixDate >= SelectedStartDate && item.StartFixDate <= SelectedEndDate);
+            if (SelectedTestMachingType != null)
+            {
+                SelectedFile = SelectedFile.Where(item => item.TestMachingType.Contains(SelectedTestMachingType));
+            }
+            if(SelectedBoardName != null) 
+            {
+                SelectedFile = SelectedFile.Where(item => item.BoardName.Contains(SelectedBoardName));
+            }
+            if (SelectedFixWay != null)
+            {
+                SelectedFile = SelectedFile.Where(item => item.FixWay.Contains(SelectedFixWay));
+            }
+            SearchedFixFile = SelectedFile.ToList();
+        }
+
+    }
 
 
     [RelayCommand]
